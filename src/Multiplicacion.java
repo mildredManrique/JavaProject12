9e37fee844ccaf634f7286acd2a4fdf30a673edf
mildredManrique <47707022+mildredManrique@users.noreply.github.com>
2019-04-01 21:20:43 -0600

    Initial commit

---
+++ src/Multiplicacion.java
@@ -0,0 +1,14 @@
+public class Multiplicacion {
+
+    public static void main (String args []) {
+
+        int a = 9;
+        int b = 9;
+        int mult = a * b;
+
+                System.out.printf("El resultado de la multiplicación es: " + mult);
+
+    }
+
+
+}
